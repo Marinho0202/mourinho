91c4dd78a387ad71cebac48efe5e27fc3340da92
Marinho0202 <karloseduardo.pa@gmail.com>
2024-10-22 22:42:55 -0300

    Algumas mudanças da página de cadastro

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -28,78 +28,93 @@ function Cadastro() {
 
   return (
     <div className="cadastro-container">
-      <h2>Cadastro de Jogador</h2>
-      <form className="cadastro-form" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="nome">Nome</label>
-          <input
-            type="text"
-            id="nome"
-            name="nome"
-            value={formData.nome}
-            onChange={handleChange}
-            required
-          />
+      <div className="cadastro-form-container">
+        <div className="logo">
+          {/* Substitua pelo caminho da sua logo */}
+          <img src="./img/logonareia.jpg" className="logo-img" />
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="telefone">Telefone</label>
-          <input
-            type="tel"
-            id="telefone"
-            name="telefone"
-            value={formData.telefone}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="senha">Senha</label>
-          <input
-            type="password"
-            id="senha"
-            name="senha"
-            value={formData.senha}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="dataNascimento">Data de Nascimento</label>
-          <input
-            type="date"
-            id="dataNascimento"
-            name="dataNascimento"
-            value={formData.dataNascimento}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="genero">Gênero</label>
-          <select
-            id="genero"
-            name="genero"
-            value={formData.genero}
-            onChange={handleChange}
-          >
-            <option value="Masculino">Masculino</option>
-            <option value="Feminino">Feminino</option>
-            <option value="Outro">Outro</option>
-          </select>
-        </div>
-        <button type="submit" className="btn-cadastrar">Cadastrar</button>
-      </form>
+        <h2>Cadastro de Jogador</h2>
+        <p>Vamos começar!</p>
+        <form className="cadastro-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="nome">Nome</label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              placeholder="Nome completo"
+              value={formData.nome}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Seu melhor e-mail"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="telefone">Telefone</label>
+            <input
+              type="tel"
+              id="telefone"
+              name="telefone"
+              placeholder="(XX) x xxxx-xxxx"
+              value={formData.telefone}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              name="senha"
+              placeholder="********"
+              value={formData.senha}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="dataNascimento">Data de Nascimento</label>
+            <input
+              type="date"
+              id="dataNascimento"
+              name="dataNascimento"
+              value={formData.dataNascimento}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="genero">Gênero</label>
+            <select
+              id="genero"
+              name="genero"
+              value={formData.genero}
+              onChange={handleChange}
+            >
+              <option value="Masculino">Masculino</option>
+              <option value="Feminino">Feminino</option>
+              <option value="Outro">Outro</option>
+            </select>
+          </div>
+          <button type="submit" className="btn-cadastrar">Cadastrar</button>
+        </form>
+      </div>
+      <div className="cadastro-image">
+        {/* A imagem lateral será exibida aqui */}
+        <img src="./img/cold.png"  className="image-lateral" />
+      </div>
     </div>
   );
 }
